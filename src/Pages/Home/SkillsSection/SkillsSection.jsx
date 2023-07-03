27ffb29aa67c77bb4aff41b552d84bfe048ead08
eshansaif <35582062+eshansaif@users.noreply.github.com>
2023-07-03 22:30:31 +0600

    my skills section is done

--- src/Pages/Home/SkillsSection/SkillsSection.jsx
+++ src/Pages/Home/SkillsSection/SkillsSection.jsx
@@ -1,5 +1,9 @@
 import Marquee from "react-fast-marquee";
 import { motion } from "framer-motion";
+import { FaBootstrap, FaCss3Alt, FaHtml5, FaReact } from 'react-icons/fa'
+import { SiDaisyui, SiExpress, SiFirebase, SiMongodb, SiMysql, SiTailwindcss } from 'react-icons/Si'
+import { RiJavascriptFill } from 'react-icons/ri'
+import { BiLogoNodejs } from 'react-icons/bi';
 
 const SkillsSection = () => {
   return (
@@ -9,272 +13,59 @@ const SkillsSection = () => {
           My Skills
         </h1>
       </div>
-      <Marquee>
-        <div className=" space-x-4">
-          <motion.button className="border-2 text-white border-[#4e9eff] px-4 py-3 hover:bg-gray-500">
-            HTML5
-          </motion.button>
-          <motion.button className="border-2 text-white border-[#4e9eff] px-4 py-3 hover:bg-gray-500">
-            CSS3
-          </motion.button>
-          <motion.button className="border-2 text-white border-[#4e9eff] px-4 py-3 hover:bg-gray-500">
-            Bootstrap
-          </motion.button>
-          <motion.button className="border-2 text-white border-[#4e9eff] px-4 py-3 hover:bg-gray-500">
-            Tailwind
-          </motion.button>
-          <motion.button className="border-2 text-white border-[#4e9eff] px-4 py-3 hover:bg-gray-500">
-            JavaScript
-          </motion.button>
-          <motion.button className="border-2 text-white border-[#4e9eff] px-4 py-3 hover:bg-gray-500">
-            React JS
-          </motion.button>
-          <motion.button className="border-2 text-white border-[#4e9eff] px-4 py-3 hover:bg-gray-500">
-            Node JS
-          </motion.button>
-          <motion.button className="border-2 text-white border-[#4e9eff] px-4 py-3 hover:bg-gray-500">
-            Express JS
-          </motion.button>
-          <motion.button className="border-2 text-white border-[#4e9eff] px-4 py-3 hover:bg-gray-500">
-            MongoDB
-          </motion.button>
-          <motion.button className="border-2 text-white border-[#4e9eff] px-4 py-3 hover:bg-gray-500">
-            Mongoose
-          </motion.button>
-          <motion.button className="border-2 text-white border-[#4e9eff] px-4 py-3 hover:bg-gray-500">
-            MVC
-          </motion.button>
-        </div>
-      </Marquee>
 
-      <div
-        className="px-6 py-4 lg:px-24 text-white"
-        data-aos="fade-up"
-        data-aos-duration="3000"
-      >
-        <div className="space-y-3">
-          <div>
-            <h1 className="text-xl font-sans font-bold">Expertise :</h1>
-            <div className="space-x-4 space-y-3 text-start">
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                HTML5
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                CSS3
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Bootstrap
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Tailwind
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                JavaScript
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                ES6
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                React JS
-              </motion.button>
-            </div>
-          </div>
-          <div>
-            <h1 className="text-xl font-sans font-bold">Comfortable : </h1>
-            <div className="space-x-4 text-start space-y-3">
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Node JS
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Express JS
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                MongoDB
-              </motion.button>
-            </div>
-          </div>
-          <div>
-            <h1 className="text-xl font-sans font-bold">Familiar with : </h1>
-            <div className="space-x-4 text-start space-y-3">
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Next js
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                MVC
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Mongoose
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Redux
-              </motion.button>
-            </div>
-          </div>
-          <div>
-            <h1 className="text-xl font-sans font-bold">Tools :</h1>
-            <div className="space-x-4 text-start space-y-3">
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Visual Studio
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Chrome dev Tool
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Netlify
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Figma
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                Firebase
-              </motion.button>
-              <motion.button
-                className="border-2 text-white border-[#4e9eff] px-2 py-1 lg:px-3 lg:py-2 hover:bg-gray-500"
-                whileHover={{
-                  scale: 1.2,
-                  //textShadow: "0px 0px 8px rgb(255,255,255)",
-                  boxShadow: "0px 0px 8px rgb(255,255,255)",
-                }}
-              >
-                GitHub
-              </motion.button>
-            </div>
-          </div>
+      <section className='text-white grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6'>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <FaHtml5 size={150} className='  '></FaHtml5>
+          <h3 className=' text-center'>HTML5</h3>
         </div>
-      </div>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <FaCss3Alt size={150} className=' text-cyan-500 ' />
+          <h2>CSS3</h2>
+        </div>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <FaBootstrap size={150} className=' text-orange-400 ' />
+          <h2>Bootstrap</h2>
+        </div>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <SiTailwindcss size={150} className=' text-green-400 ' />
+          <h2>Tailwind CSS</h2>
+        </div>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <SiDaisyui size={150} className=' text-orange-400 ' />
+          <h2>DaisyUI</h2>
+        </div>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <RiJavascriptFill size={150} className=' text-orange-300 ' />
+          <h2>JavaScript</h2>
+        </div>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <BiLogoNodejs size={150} className=' text-sky-600 ' />
+          <h2>Node JS</h2>
+        </div>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <FaReact size={150} className=' text-teal-500 ' />
+          <h2>React.js</h2>
+        </div>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <SiExpress size={150} className=' text-slate-300 ' />
+          <h2>Express JS</h2>
+        </div>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <SiMongodb size={150} className=' text-green-600' />
+          <h2>MongoDB</h2>
+        </div>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <SiFirebase size={150} className=' text-amber-400 ' />
+          <h2>Firebase</h2>
+        </div>
+        <div className=' p-8 shadow-2xl flex flex-col items-center'>
+          <SiMysql size={150} className=' text-emerald-400 ' />
+          <h2>MySql</h2>
+        </div>
+
+
+      </section>
     </div>
   );
 };
